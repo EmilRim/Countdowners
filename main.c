ab8c56c3a0496fb11ca1ef78efb92942e4ab4306
Bugne <pacevichiute@gmail.com>
2024-12-31 09:35:21 +0200

    Calendar day number function added

--- main.c
+++ main.c
@@ -6,13 +6,23 @@
 int main()
 {
     char title[255];
+    int number; //number of days
     printf("maine\n");
-    FILE *file = openFile("test.html");
+    FILE * file = openFile("test.html");
     headComponents(file);
     fprintf(file, "\n  </head>");
     printf("kokio title nori?\n");
     fscanf(stdin, "%255[^\n]", &title);
     body(file, title);
+    printf("kiek dienu?\n");
+    fscanf(stdin, "%d", &number);
+
+    while(number < 1 || number > 30)
+    {
+        printf("Please enter a number in a range [1; 30]: ");
+        fscanf(stdin, "%d", &number);
+    }
+    dayCounter(file, number);
 
     fprintf(file, "\n</main>\n</body>");
     fprintf(file, "\n</html>");
@@ -28,4 +38,5 @@ int main()
 #endif
 
     return 0;
+
 }
